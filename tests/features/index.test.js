7df98113c94439ed275342108bc3b526f78c8eb4
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-03-31 20:51:56 +0300

    It has a welcome message

--- tests/features/index.test.js
+++ tests/features/index.test.js
@@ -1,9 +1,13 @@
-// const fs = require('fs');
-
 describe('Poems index page', () => {
   it('has title Poems', async () => {
     await page.goto('http://localhost:3000');
 
     await expect(page).toMatchElement('title', {text: 'Poems'})
+  });
+
+  it('welcomes the user', async () => {
+    await page.goto('http://localhost:3000');
+
+    await expect(page).toMatchElement('p', { text: 'Welcome to Poems'})
   })
 })
