fc7465ce0472eab47d4666ddbe5d436340fe1e69
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-04-08 19:35:45 +0300

    User is informed what the page is about

--- tests/features/index.test.js
+++ tests/features/index.test.js
@@ -1,13 +1,20 @@
 describe('Poems index page', () => {
   it('has title Poems', async () => {
     await page.goto('http://localhost:3000');
-
-    await expect(page).toMatchElement('title', {text: 'Poems'})
+    
+    const pageTitle = await page.title('http://localhost:3000');
+    
+    await expect(pageTitle).toBe('Poems')
   });
 
   it('welcomes the user', async () => {
     await page.goto('http://localhost:3000');
 
     await expect(page).toMatchElement('p', { text: 'Welcome to Poems'})
+  })
+  it('grabs the poems id element', async () => {
+    await page.goto('http://localhost:3000');
+  
+    await expect(page).toMatchElement('#poems', { text: 'Here you\'ll see poems' })
   })
 })
