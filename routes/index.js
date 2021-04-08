df7394a8455038c5ee47930d4fade5283a65fa6b
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-04-08 19:38:12 +0300

    Pass message text to route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Poems' });
+  res.render('index', { 
+    title: 'Poems',
+    message: 'Here you\'ll see poems' });
 });
 
 module.exports = router;
