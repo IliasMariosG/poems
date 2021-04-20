8079b25712144d3399c10b4db93a70c4e965f014
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-04-20 13:33:16 +0300

    allPoems function returns all poems

--- tests/models/poem.test.js
+++ tests/models/poem.test.js
@@ -3,15 +3,15 @@ const client = require('../../db/dbConfig');
 
 describe('Poem', () => {
   describe('#allPoems', () => {
-    it('returns all poems', () => {
-      client.connect();
-      client.query(`INSERT INTO poems VALUES(1, All that is gold does not glitter)`)
-      var poemsAll = poems.allPoems
-      
-      expect(poemsAll).toContain("All that is gold does not glitter")
-      expect(poemsAll).toContain("Shall be")
-      expect(poemsAll).toContain("It is a sport")
-      expect(poemsAll).toContain("The kid went on to play outside")
-    })
-  })
-})
+    it('returns all poems', (done) => {
+
+      poems.allPoems((allPoems) => {
+        expect(allPoems[0].content).toContain("All that is gold does not glitter")
+        expect(allPoems[1].content).toContain("Shall be")
+        expect(allPoems[2].content).toContain("It is a sport")
+        expect(allPoems[3].content).toContain("The kid went on to play outside")
+        done();
+      })
+    });
+  });
+});
