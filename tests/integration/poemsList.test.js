372d32f2387baeef14d868d6470925fe20a05b80
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-04-28 21:36:56 +0300

    Poems list page has a status of 200

---
+++ tests/integration/poemsList.test.js
@@ -0,0 +1,9 @@
+describe('Poems page', () => {
+  it('return a status of 200', async () => {
+    const response = await page.goto('http://localhost:3000/poems');
+    actualResponseStatus = response.status();
+    expectedResponseStatus = 200;
+
+    await expect(actualResponseStatus).toEqual(expectedResponseStatus);
+  });
+})
