ba5933b87660be6ee547af55e3952fbcd45fa5f8
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-05-04 22:16:54 +0300

    Tests works when running alone
    Needs proper configuration when all the suites are running

--- tests/integration/addPoem.test.js
+++ tests/integration/addPoem.test.js
@@ -1,15 +1,14 @@
 describe('add a poem', () => {
-  beforeEach( () => {
-    jest.setTimeout(6000);
-  })
-  it('renders the new page with the poem', async () => {
-    await page.goto('http://localhost:3000/poems/new');
-    await page.type('textarea', 'This is my first poem');
-   
-    await page.click('button')
-    await page.waitForNavigation();
-    await page.screenshot({ path: 'tmp/screenshot.png' });
+  beforeAll( async() => {
+    jest.setTimeout(16000);
+      await page.goto('http://localhost:3000/poems/new');
+  });
+  it('renders the new page with the poem', async() => {
+      await page.type('textarea', 'This is my first poem');
 
-    await expect(page).toMatch('success: This is my first poem');
+      await page.click('button')
+      await page.waitForNavigation();
+
+      await expect(page).toMatch('Success. Poem added');
   });
 })
