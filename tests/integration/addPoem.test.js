285c55c272f36451d19d266fdc5f8fda637bc79a
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-09-20 11:56:23 +0100

    It can resolve unhandled promise

--- tests/integration/addPoem.test.js
+++ tests/integration/addPoem.test.js
@@ -1,14 +1,21 @@
 describe('add a poem', () => {
-  beforeAll( async() => {
-    jest.setTimeout(16000);
+  beforeAll(async () => {
+    jest.setTimeout(5000);
       await page.goto('http://localhost:3000/poems/new');
   });
-  it('renders the new page with the poem', async() => {
-      await page.type('textarea', 'This is my first poem');
+  it('renders the new page with the poem', async () => {
+      try {
+        await Promise.all([
+          page.type('textarea', 'This is my first poem'),
 
-      await page.click('button')
-      await page.waitForNavigation();
+          page.click('button'),
+          page.waitForNavigation(),
+        ]),
 
-      await expect(page).toMatch('Success. Poem added');
+        await expect(page).toMatch('Success. Poem added');
+      } catch (err) {
+        console.log(err);
+        await page.close();
+      }
   });
 })
