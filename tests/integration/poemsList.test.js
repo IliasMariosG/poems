a6604fedfbd0100383143c9543f3697636164066
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-09-20 11:57:31 +0100

    Test takes into account 304 response status

--- tests/integration/poemsList.test.js
+++ tests/integration/poemsList.test.js
@@ -2,7 +2,7 @@ describe('Poems page', () => {
   it('return a status of 200', async () => {
     const response = await page.goto('http://localhost:3000/poems');
     actualResponseStatus = response.status();
-    expectedResponseStatus = 200;
+    expectedResponseStatus = 200 || 304;
 
     await expect(actualResponseStatus).toEqual(expectedResponseStatus);
   });
