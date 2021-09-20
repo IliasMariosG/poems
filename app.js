78803ab750edb669f1face152745e8898450ad1d
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-09-20 10:39:33 +0100

    Body styling is connected

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// using app.use to serve up static CSS files in public/assets/folder
+// when /public link is called in ejs files.
+// app.use("/route", express.static("foldername"));
+app.use('/public', express.static('public'));
 app.use(errorhandler());
 
 app.use('/', indexRouter);
